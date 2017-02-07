b1719fbc237b4303a09e2d5c570fa2898e5b5cc6
Matthew Holman <mateoholman@gmail.com>
2017-02-07 15:49:32 -0600

    Setup structure & added router

---
+++ client/components/Single.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Single = React.createClass({
+  render() {
+    return(
+      <div>
+        I'm the Single.
+      </div>
+    );
+  }
+});
+
+export default Single;
