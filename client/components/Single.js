522f4dfbfaba59f03ec110cf0f1574f7c2a17818
Matthew Holman <mateoholman@gmail.com>
2017-02-08 21:33:33 -0600

    Created Comments component and reducer

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Photo from './Photo';
-//import Comments from './Comments';
+import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
-    const index = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    const { postId } = this.props.params
+    const index = this.props.posts.findIndex((post) => post.code === postId);
     const post = this.props.posts[index];
+    const postComments = this.props.comments[postId] || [];
     return(
-      <div>
+      <div className="single-photo">
         <Photo index={index} post={post} {...this.props} />
+        <Comments postComments={postComments} {...this.props} />
       </div>
     );
   }
--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,4 +1,25 @@
-export default function comments(state = [], action){
-  console.log(state, action);
+function postComments(state = [], action){
+  switch(action.type){
+    case 'ADD_COMMENT':
+      //return the new state with the new comment
+      return [...state, {
+        user: action.author,
+        text: action.comment
+      }];
+    case 'REMOVE_COMMENT' : return state;
+    default : return state;
+  }
   return state;
 }
+
+function comments(state = [], action){
+  if (typeof action.postId !== 'undefined'){
+    return {
+      ...state,
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
+  return state;
+}
+
+export default comments;
