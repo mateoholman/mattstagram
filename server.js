b887fd0bf4b44b684a1807bab386b1761e721382
Matthew Holman <mateoholman@gmail.com>
2017-05-31 15:48:56 -0500

    Removed bugs for production

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-+const path = require('path');
- +const express = require('express');
- +const setupMiddleware = require('./serverMiddleware');
- +require('dotenv').config();
- +
- +const app = express();
- +
- +// Apply the right middlewares for if we're in dev or prod mode
- +setupMiddleware(app);
- +
- +app.get('*', (req, res) => {
- +  res.sendFile(path.join(__dirname, 'index.html'));
- +});
- +
- +app.set('port', process.env.PORT || 7770);
- +
- +const server = app.listen(app.get('port'), () => {
- +  console.log(`Express running → PORT ${server.address().port}`);
- +});
+const path = require('path');
+ const express = require('express');
+ const setupMiddleware = require('./serverMiddleware');
+ require('dotenv').config();
+
+ const app = express();
+
+ // Apply the right middlewares for if we're in dev or prod mode
+ setupMiddleware(app);
+
+ app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname, 'index.html'));
+ });
+
+ app.set('port', process.env.PORT || 7770);
+
+ const server = app.listen(app.get('port'), () => {
+   console.log(`Express running → PORT ${server.address().port}`);
+ });
